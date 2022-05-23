847db417550f9bda5e4c466734612e9e87044708
abhi-shek3 <abhishek.kumar@springml.com>
2022-05-23 19:05:00 +0530

    minor css fixes

--- angular/src/app/annotate/annotate.component.ts
+++ angular/src/app/annotate/annotate.component.ts
@@ -9,25 +9,25 @@ import { ApiService } from '../api.service';
 })
 export class AnnotateComponent implements OnInit {
 
-  constructor(private service: ApiService, private sanitizer: DomSanitizer) {
-  }
+  constructor(private service: ApiService, private sanitizer: DomSanitizer) { }
+
   loadingAnimate = true;
   imagePath: any = ['',''];
 
   ngOnInit(): void {
     this.loadingAnimate = true;
     this.service.getFrames().subscribe({
-        next: (res:any) => {
-          this.loadingAnimate = false;
-          this.imagePath = [];
-          console.log(res);
-          res['data'].forEach((element:any) => {
-            this.imagePath.push(this.sanitizer.bypassSecurityTrustUrl(`data:image/jpeg;base64,${element}`))
-          });
-        },
-        error: (err:any) => this.loadingAnimate = false
-      })
+      next: (res:any) => {
+        this.loadingAnimate = false;
+        this.imagePath = [];
+        console.log(res);
+        res['data'].forEach((element:any) => {
+          this.imagePath.push(this.sanitizer.bypassSecurityTrustUrl(`data:image/jpeg;base64,${element}`))
+        });
+      },
+      error: err => this.loadingAnimate = false
+    });
   }
 
-
+  
 }
